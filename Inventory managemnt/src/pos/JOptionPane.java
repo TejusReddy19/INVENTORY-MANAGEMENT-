32a2843f89cf4bc147f3905a1a5f5977597e6cf6
TejusReddy19 <109446973+TejusReddy19@users.noreply.github.com>
2023-08-15 20:49:27 +0530

    final upload

---
+++ Inventory managemnt/src/pos/JOptionPane.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pos;
+
+/**
+ *
+ * @author tejus
+ */
+class JOptionPane {
+
+    static void showMessageDialog(Object object, String username_and_password_do_not_matched) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
